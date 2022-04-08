f1f422cd75bcb55ac366dfcbadbe1339210cc581
maplerichie <babierichie@hotmail.com>
2022-04-08 13:54:40 +0800

    Fix CORS

--- lib/cors.ts
+++ lib/cors.ts
@@ -1,10 +1,12 @@
 import Cors from "cors";
 
-var whitelist = [
+var originWl = [
   "http://localhost:4000",
   "https://apeuniverse.xyz",
   "https://www.apeuniverse.xyz",
 ];
+
+var hostWl = ["localhost:4000", "apeuniverse.xyz", "www.apeuniverse.xyz"];
 
 function initMiddleware(middleware) {
   return (req, res) =>
@@ -13,15 +15,14 @@ function initMiddleware(middleware) {
         const origin = req.headers.origin;
         const host = req.headers.host;
         if (origin === undefined) {
-          if (host !== "localhost:4000" && req.headers["api-key"] !== "apepy") {
+          if (hostWl.indexOf(host) < 0 && req.headers["api-key"] !== "apepy") {
             return reject("403 Forbidden");
           }
         } else {
-          if (whitelist.indexOf(origin) < 0) {
+          if (originWl.indexOf(origin) < 0) {
             return reject("403 Forbidden");
           }
         }
-        // if (whitelist.indexOf(req.header('Origin')) !== -1) {
         if (result instanceof Error) {
           return reject(result);
         }
